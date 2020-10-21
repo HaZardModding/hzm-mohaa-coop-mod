3f713ca7aac9e879073b6cafd011c41c21891ef7
chrissstrahl <chrissstrahl@yahoo.de>
2020-10-21 17:34:49 +0200

    added ::set3rdPerson changed netname data extraction to use pattern whithespace+colon+number+data

--- ui/coop_fov/generateFovCfgFiles.bat
+++ ui/coop_fov/generateFovCfgFiles.bat
@@ -4,8 +4,9 @@ set /a IhateBatch=80
 set /a EveryoneHatesBatch=142
 set /a HowManyMoreHateBatchEachSecond=2
 :fuckBatch
+REM 01 is fov datatype keyword in coop
 if %IhateBatch% lss %EveryoneHatesBatch% (
-	echo seta g_m2l2 %IhateBatch%;seta g_m2l1 "append name #fov,%IhateBatch%";wait 1000000000000000000090000;vstr g_m2l1;>%IhateBatch%.cfg
+	echo seta g_m2l2 %IhateBatch%;seta g_m2l1 "append name ,1%IhateBatch%";vstr g_m2l1; //whitespace+,+number are identifiers, the numbers following are the fov to set>%IhateBatch%.cfg
 	set /a IhateBatch+=2
 	goto fuckBatch
 )
